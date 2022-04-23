e658fb519fad28e36088301867e6eee2a77ef849
Maxframe <wrnerm@gmail.com>
2022-04-23 23:56:50 +0200

    push of the night

--- pages/process/w1-fri.js
+++ pages/process/w1-fri.js
@@ -1,4 +1,5 @@
 import NavigationProcessDay from "../../components/NavigationProcessDay";
+import Image from "next/image";
 
 export default function ProcessDay(props) {
 
@@ -6,73 +7,65 @@ export default function ProcessDay(props) {
     {
       day: "Monday",
       link: "day1",
-      color: "emerald"
     },
     {
       day: "Tuesday",
       link: "day2",
-      color: "red"
-    }
+    },
+    {
+      day: "Wednesday",
+      link: "day3",
+    },
+    {
+      day: "Thursday",
+      link: "day4",
+    },
+    {
+      day: "Friday",
+      link: "day5",
+    },
   ];
-  const listItems = data.map((loc, index) => (
-    <li className="writing-mode-vertical-rl text-6xl whitespace-nowrap origin-top-left -rotate-45" key={index}>
-      {loc}
-    </li>  
-    ));
+  const dayNavigation = data.map((weekday, index) => (
+    <div className="  block group w-32" key={index}>
+      <a
+        className="relative inline-block focus:outline-none focus:ring w-32 text-center"
+        href={`#${weekday.link}`}
+      >
+        <span className={`absolute inset-0 transition translate-x-0 translate-y-0 group-hover:translate-y-1.5 group-hover:translate-x-1.5 bg-yellow-700 group-hover:bg-yellow-500`}></span>
+
+        <span className="relative inline-block py-3 text-sm font-bold tracking-widest uppercase border-2 border-current w-32">
+          {weekday.day}
+        </span>
+      </a>
+    </div>
+  ));
+
+  const dayContent = data.map((weekday, index) => (
+    <div className=" block group w-32" key={index}>
+        <span className={`absolute  bg-yellow-700 `}></span>
+
+        <span className="relative inline-block py-3 text-sm font-bold tracking-widest uppercase border-2 border-current w-32">
+          {weekday.day}
+        </span>
+    </div>
+  ));
+
   return (
     <>
       <NavigationProcessDay title={"Week 1 | Friday"} />
-      <div className="pt-12 space-y-4">
-       
-        <div className="block w-fit">
-          <a
-            className="relative inline-block group focus:outline-none focus:ring"
-            href="#day2"
-          >
-            <span className="absolute inset-0 transition translate-x-0 translate-y-0 bg-emerald-700 group-hover:translate-y-1.5 group-hover:translate-x-1.5 group-hover:bg-emerald-500"></span>
-
-            <span className="relative inline-block px-8 py-3 text-sm font-bold tracking-widest uppercase border-2 border-current">
-              Monday
-            </span>
-          </a>
+      <div className="pt-12 flex">
+        <div className="fixed space-y-4">
+          {dayNavigation}
         </div>
-        <div className="block w-fit">
-          <a
-            className="relative inline-block group focus:outline-none focus:ring"
-            href="#day2"
-          >
-            <span className="absolute inset-0 transition translate-x-0 translate-y-0 bg-emerald-700 group-hover:translate-y-1.5 group-hover:translate-x-1.5 group-hover:bg-emerald-500"></span>
-
-            <span className="relative inline-block px-8 py-3 text-sm font-bold tracking-widest uppercase border-2 border-current">
-              Tuesday
-            </span>
-          </a>
+        <div className="ml-64 space-y-4">
+          {dayContent}
+          <Image src="/../../public/img/wi-fri-weekdays/1_monday.jpg" width="300px" layout="fill"/>   
+          
         </div>
 
-
       </div>
 
-      <div id="day1">
-        <p>something about this day</p>
-      </div>
-      <div id="day2">
-        <p>something about this day</p>
-      </div>
-      <div id="day3">
-        <p>something about this day</p>
-      </div>
-      <div id="day4">
-        <p>something about this day</p>
-      </div>
-      <div id="day5">
-        <p>something about this day</p>
-      </div>
-      <div id="day6">
-        <p>something about this day</p>
-      </div>
-      <div id="day7">
-        <p>something about this day</p>
-      </div>
+
     </>
   );
 }
